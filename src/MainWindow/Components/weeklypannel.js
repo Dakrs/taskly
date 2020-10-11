5b78d973d7d5a86497e47fc08ca8392adf482f3c
Dakrs <diogosobral98@gmail.com>
2020-10-11 02:18:12 +0100

    weekly add modal

--- src/MainWindow/Components/weeklypannel.js
+++ src/MainWindow/Components/weeklypannel.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import Vue from 'vue';
+import Datepicker from 'vuejs-datepicker';
 
 
 Vue.component('weekly-pannel',{
@@ -24,6 +25,11 @@ Vue.component('weekly-pannel',{
 
     return {
       cards: cards
+    }
+  },
+  methods: {
+    triggerModal: function (){
+      $('#weekly-add-modal').modal('show');
     }
   },
   template: `
@@ -67,7 +73,7 @@ Vue.component('weekly-pannel',{
           </div>
         </div>
       </div>
-      <div class="card-weekly-add spaced">
+      <div @click="triggerModal()" class="card-weekly-add spaced">
         <div class="card-weekly-content">
           <div class="card-weekly-add-content-col1">
             <i style="color: white" class="fas fa-plus"></i>
@@ -78,8 +84,61 @@ Vue.component('weekly-pannel',{
         </div>
       </div>
     </div>
+    <div class="modal fade" id="weekly-add-modal" data-focus="true" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" style="-webkit-user-select: none;">
+      <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalCenterTitle">Add new Weekly Pin</h5>
+          </div>
+          <div class="modal-body">
+            <form id="ADDTODO-FORM">
+              <div class="form-row">
+                <div class="form-group col-md-6">
+                  <label for="inputTitle">Title</label>
+                  <input name="title" type="text" class="form-control" id="inputTitle" maxlength="20" required>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="inputIcon">Icon</label>
+                  <select name="icon" id="inputIcon" class="form-control">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </select>
+                </div>
+              </div>
+              <div class="form-row">
+                <div class="form-group col-md-6">
+                  <label for="inputTag">Tag</label>
+                  <select name="tag" id="inputTag" class="form-control">
+                    <option>Low</option>
+                    <option>High</option>
+                    <option>Personal</option>
+                  </select>
+                </div>
+                <div class="form-group col-md-6">
+                  <label for="inputDate">Date</label>
+                  <datepicker :bootstrap-styling="true" name="date" id="inputDate" input-class="datepickerColor" required></datepicker>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputDescription">Description</label>
+                <input name="description" type="text" class="form-control" id="inputDescription" maxlength="200">
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-dark">Send</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  `
+  `,
+  components: {
+    Datepicker
+  }
 })
 
 /**
